a1211abf61e26fe263dcb59f7355c65533fe6cf5
Sean McLeaish <smcleaish@gmail.com>
2024-11-15 08:01:40 -1000

    2.2

---
+++ 2.big_O/2.O_n_squared.py
@@ -0,0 +1,15 @@
+## WRITE THE PRINT_ITEMS FUNCTION HERE ##
+#                                       #
+#                                       #
+#                                       #
+#                                       #
+#########################################
+def print_items(n):
+    x = range(n)
+    for i in x:
+        for j in x:
+            print(i, j)
+
+
+# DO NOT CHANGE THIS LINE:
+print_items(10)
